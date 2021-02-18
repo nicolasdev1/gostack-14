875fee14ea00d02e34ae9d466673ea9dc88353af
Nicolas Carvalho <nicolaspessoal@icloud.com>
2021-02-18 16:45:03 -0300

    Att folders

--- lessons/github-explorer/src/pages/Dashboard/index.tsx
+++ lessons/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,26 +1,56 @@
-import { useState, FormEvent } from 'react'
-
+import { useState, useEffect, FormEvent, ChangeEvent } from 'react'
+import { Link } from 'react-router-dom'
 import { FiChevronRight } from 'react-icons/fi'
 
 import api from '../../services/api'
 
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 import logoImg from '../../assets/logo.svg'
 
 interface Repository {
-    
+    full_name: string
+    description: string
+    owner: {
+        login: string
+        avatar_url: string
+    }
 }
 
 const Dashboard: React.FC = () => {
-    const [newRepository, setNewRepository] = useState('')
-    const [repositories, setRepositories] = useState([])
+    const [newRepository, setNewRepository] = useState<string>('')
+    const [inputError, setInputError] = useState<string>('')
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+
+        if (!storagedRepositories) return []
+
+        return JSON.parse(storagedRepositories)
+    })
+
+    useEffect(() => {
+        localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories))
+    }, [repositories])
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         event.preventDefault()
+        
+        if (!newRepository) {
+            return setInputError('Digite o autor/nome do repositório.')
+        }
 
-        const { data } = await api.get(`repos/${newRepository}`)
+        try {
+            const { data } = await api.get<Repository>(`repos/${newRepository}`)
 
-        setRepositories([...repositories, data])
+            setRepositories([...repositories, data])
+            setNewRepository('')
+        } catch {
+            setInputError('Ocorreu um erro ao buscar o repositório.')
+        }
+    }
+
+    function onChange(event: ChangeEvent<HTMLInputElement>): void {
+        setNewRepository(event.target.value)
+        setInputError('')
     }
 
     return (
@@ -28,28 +58,33 @@ const Dashboard: React.FC = () => {
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form onSubmit={handleAddRepository}>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
                 <input
                     value={newRepository}
-                    onChange={event => setNewRepository(event.target.value)}
-                    placeholder="Digite o nome do repositório"
+                    onChange={onChange}
+                    placeholder="Digite o nome/autor do repositório"
+                    autoComplete="none"
                 />
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            { inputError && <Error>{inputError}</Error> }
+
             <Repositories>
-                <a href="test">
-                    <img
-                        src="https://avatars.githubusercontent.com/u/56241288?s=460&u=8641d9b9ac08aa43eebac41b25bac5c6f3f6bf9e&v=4"
-                        alt="Nícolas Carvalho"
-                    />
-                    <div>
-                        <strong>nicolasdev1/clean-architecture-app</strong>
-                        <p>Swift application using Test-Driven Development (TDD) as a methodology.</p>
-                    </div>
-                    
-                    <FiChevronRight size={20} />
-                </a>
+                {repositories.map(repository => (
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        
+                        <FiChevronRight size={20} />
+                    </Link>
+                ))}
             </Repositories>
         </>
     )
--- lessons/github-explorer/src/pages/Dashboard/styles.ts
+++ lessons/github-explorer/src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { shade } from 'polished'
+
+interface FormProps {
+    hasError: boolean
+}
 
 export const Title = styled.h1`
     font-size: 48px;
@@ -9,16 +13,22 @@ export const Title = styled.h1`
     margin-top: 80px;
 `
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
     display: flex;
 
     > input {
         flex: 1;
-        padding: 0 24px;
+        padding: 0 22px;
         border-radius: 5px 0 0 5px;
         color: #3a3a3a;
+        border: 2px solid #fff;
+        border-right: 0;
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        `}
 
         &::placeholder {
             color: #a8a8b3;
@@ -32,16 +42,22 @@ export const Form = styled.form`
         color: #fff;
         font-weight: bold;
         transition: background-color .2s;
+        border: 0;
 
         &:hover {
-            background: ${shade(0.2, '#04d361')}
+            background: ${shade(0.15, '#04d361')}
         }
     }
 
     > input, button {
-        height: 70px;
-        border: 0;
+        height: 65px;
     }
+`
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `
 
 export const Repositories = styled.div`
@@ -73,7 +89,8 @@ export const Repositories = styled.div`
         }
 
         > div {
-            margin-left: 16px;
+            margin: 0 16px;
+            flex: 1;
             
             > strong {
                 font-size: 20px;
