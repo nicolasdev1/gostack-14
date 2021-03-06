0ce034b309420bf790fb1208281639824c79a4c7
nicolasdev1 <nicolaspessoal@icloud.com>
2021-03-06 19:25:43 -0300

    feat: add level-04 and level-05 challenges

---
+++ challenges/level-04/database-relations/src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,98 @@
+import { inject, injectable } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import Order from '../infra/typeorm/entities/Order';
+import IOrdersRepository from '../repositories/IOrdersRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+@injectable()
+class CreateOrderService {
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
+
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with the given id.');
+    }
+
+    const existentProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (!existentProducts.length) {
+      throw new AppError('Could not find any products with the given ids.');
+    }
+
+    const existentProductsIds = existentProducts.map(product => product.id);
+
+    const inexistentProducts = products.filter(
+      product => !existentProductsIds.includes(product.id),
+    );
+
+    if (inexistentProducts.length) {
+      throw new AppError(`Could not find product ${inexistentProducts[0].id}.`);
+    }
+
+    const productsWithNoQuantitiesAvailable = products.filter(
+      product =>
+        existentProducts.filter(
+          existentProduct => existentProduct.id === product.id,
+        )[0].quantity < product.quantity,
+    );
+
+    if (productsWithNoQuantitiesAvailable.length) {
+      throw new AppError(
+        `The quantity ${productsWithNoQuantitiesAvailable[0].quantity} is not available for ${productsWithNoQuantitiesAvailable[0].id}.`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existentProducts.filter(
+        existentProduct => existentProduct.id === product.id,
+      )[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existentProducts.filter(
+          existentProduct => existentProduct.id === product.product_id,
+        )[0].quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
+  }
+}
+
+export default CreateOrderService;
---
+++ challenges/level-04/database-relations/src/shared/infra/http/server.ts
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(5555, () => {
+  console.log('🚀 Server started on port 5555!');
+});
